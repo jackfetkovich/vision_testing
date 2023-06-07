c6f0ba0c3d71e97eb246d14bec85df9758856a79
jackfetkovich <johnfetkovich1014@gmail.com>
2023-06-07 18:50:38 -0400

    refactor

--- one-D-convolution.py
+++ one-D-convolution.py
@@ -32,32 +32,37 @@ def identify_maxima(peaks, FLAT_THRESH = 200, HEIGHT_THRESH = 10000, WIDTH_THRES
     i+= k if k >=1 else 1
 
   return maxima
+def locate_poles(input):
+  img = cv2.cvtColor(input, cv2.COLOR_BGR2HLS)
 
-input = cv2.imread('./multiple_poles.png')
-img = cv2.cvtColor(input, cv2.COLOR_BGR2HLS)
+  lowHLS = np.array([0, 52.4, 42.5]) # low color bound
+  highHLS = np.array([26.9, 255, 255]) # high color bound
 
-lowHLS = np.array([0, 52.4, 42.5]) # low color bound
-highHLS = np.array([26.9, 255, 255]) # high color bound
+  mask = cv2.inRange(img, lowHLS, highHLS) # perform the mask
+  mask = cv2.medianBlur(mask, 7) # noise filtering
 
-mask = cv2.inRange(img, lowHLS, highHLS) # perform the mask
-mask = cv2.medianBlur(mask, 7) # noise filtering
+  # 1D Convolution Time
+  cols = mask.shape[1]
+  rows = mask.shape[0]
 
-# 1D Convolution Time
-cols = mask.shape[1]
-rows = mask.shape[0]
+  peaks = np.zeros(cols)
 
-peaks = np.zeros(cols)
+  for col in range(cols): # This is the actual convolution
+      sum = 0
+      for row in range(rows):
+        sum += mask[row][col]
+      peaks[col] = sum
 
-for col in range(cols): # This is the actual convolution
-    sum = 0
-    for row in range(rows):
-      sum += mask[row][col]
-    peaks[col] = sum
+  maxima = identify_maxima(peaks)
 
-maxima = identify_maxima(peaks)
+  for max in maxima:
+    cv2.line(input, (max, 0), (max, mask.shape[0]), (255, 52, 175), thickness=2)
 
-for max in maxima:
-  cv2.line(input, (max, 0), (max, mask.shape[0]), (255, 52, 175), thickness=2)
+  return input
 
-cv2.imshow("image", input)
+input = cv2.imread('./media/multiple_poles.png')
+out = locate_poles(input)
+cv2.imshow("out",out)
 cv2.waitKey(0)
+
+
